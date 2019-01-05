c3b1634892ee9c0a10f1699c5ddb701c37da8ae0
Jimmy Situ <web@jimmystone.cn>
2019-01-05 21:23:42 +0800

    Add plot for power graph

---
+++ plot4mcpat.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3 -u
+
+from optparse import OptionParser
+import matplotlib.pyplot as plt
+import re
+
+if __name__ == "__main__":
+    parser = OptionParser()
+
+    parser.add_option("-p", "--power", dest="pwr",
+                    help="Power log file from mcpat")
+
+    (opts, args) = parser.parse_args()
+    fp = open(opts.pwr, 'r')
+
+    powerStats = []
+    line = fp.readline()
+    while(line):
+        m = re.match(r'^  Runtime Dynamic = (\S+) W', line)
+        if m:
+            powerStats.append(m.group(1))
+        line = fp.readline()
+
+    fp.close()
+
+    plt.plot(powerStats)
+    plt.ylabel("Power(W)")
+    plt.xlabel("Time(ms)")
+    exit(0)
